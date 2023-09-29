2d8a0279893ed5d92b6fdad3d1b45b40b18823f2
Benjamin Thompson <bthompson@uc.cl>
2023-09-29 20:47:39 -0300

    ModelosControladores

---
+++ app/controllers/solicitud_controller.rb
@@ -0,0 +1,2 @@
+class SolicitudController < ApplicationController
+end
---
+++ test/controllers/mensaje_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class MensajeControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
