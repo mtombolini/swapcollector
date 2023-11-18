c28dc1323a2b37cc9747ecb4d1fdb935feffd8f6
benjaruz1002 <benjaruz1002@uc.cl>
2023-11-18 00:31:43 -0300

    Hello world

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'render/index'
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }, 
                    path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "render#index"
 end
