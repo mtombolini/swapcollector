fd4a7ad2019b30d11aadb874d49092e7739c4b0b
benjaruz1002 <benjaruz1002@uc.cl>
2023-11-18 23:46:04 -0300

    Modelos

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   get 'products/create'
   get 'products/update'
   get 'products/destroy'
-  get 'render/index'
   devise_for :users, controllers: { sessions: 'users/sessions', registrations: 'users/registrations' }, 
              path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
 
