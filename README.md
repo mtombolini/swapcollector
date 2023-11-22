43ea5c30a3df11d8698e3b8fc86d3283c14a7ed1
Benjamin Thompson <bthompson@uc.cl>
2023-11-21 23:06:07 -0300

    readme y testing

--- README.md
+++ README.md
@@ -3,3 +3,8 @@
 La página es una plataforma de intercambio de objetos coleccionables de todo tipo.
 Los CRUDs están completos y pueden hacerse desde la misma página (tanto para usuarios como para productos). En el inicio de la página
 se muestran todos los productos publicados para navegar a través de ellos. Los productos con una estrella en la esquina son los productos que le pertenecen al usuario actual que está en la sesión. Si el usuario clickea uno de sus productos este será dirigido a una vista para editarlo o eliminarlo. Si el usuario clickea un producto que no le pertenece, este será dirigido a una vista con los detalles del producto y le dará una opción para hacer una solicitud de intercambio. La página ofrece al usuario la opción de publicar un producto, en donde pide los siguientes parámetros: nombre(str) descripción(str) precio (float) e indice de rareza (int del 1 al 5). También le pide al usuario adjuntar una imagen del producto.
+Para utilizar la gema cloudinary sin errores y que las imágenes se muestren bien en la página es necesario poner lo siguiente en el archivo .env: CLOUDINARY_CLOUD_NAME=dwi1ya5eh
+CLOUDINARY_API_KEY=761769923978181
+CLOUDINARY_API_SECRET=q_w79xNUmY-YuPXfHfTsr3qCQHU
+También hay que tener instalada con sudo la librería libvips.
+Intentamos implementar el testing pero nos lanza errores. La implementación está en test/models/user_test.rb.
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,64 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # Test de creacion de un usuario
+
+  test "crea un usuario con exito" do
+    user = User.new(name: "Ricardo", email: "ricardo@example.com", password: "password")
+    assert user.save
+  end
+
+  # Test de validacion del nombre del usuario
+
+  test "el nombre del usuario es obligatorio" do
+    user = User.new(email: "ricardo@example.com", password: "password")
+    assert_not user.save
+    assert user.errors[:name].any?
+  end
+
+  # Test de validacion del correo electronico del usuario
+
+  test "el correo electronico del usuario es obligatorio" do
+    user = User.new(name: "Ricardo", password: "password")
+    assert_not user.save
+    assert user.errors[:email].any?
+  end
+
+
+  # Test de validacion del formato del correo electronico del usuario
+
+  test "el formato del correo electronico del usuario debe ser valido" do
+    user = User.new(name: "Ricardo", email: "ricardo@example.com", password: "password")
+    assert user.save
+  end
+
+  # Test de validacion de la contrasena del usuario
+
+  test "la contrasena del usuario es obligatoria" do
+    user = User.new(name: "Ricardo", email: "ricardo@example.com")
+    assert_not user.save
+    assert user.errors[:password].any?
+  end
+
+  # Test de validacion de la longitud de la contrasena del usuario
+
+  test "la longitud de la contrasena del usuario debe ser mayor que 6" do
+    user = User.new(name: "Ricardo", email: "ricardo@example.com", password: "password")
+    assert_not user.save
+    assert user.errors[:password].any?
+  end
+
+  # Test de la relación del usuario con los productos
+
+  test "el usuario tiene muchos productos" do
+    user = User.new(name: "Ricardo", email: "ricardo@example.com", password: "password")
+    assert user.save
+
+    product1 = Product.new(name: "Producto 1")
+    product2 = Product.new(name: "Producto 2")
+    user.products << product1
+    user.products << product2
+
+    assert_equal 2, user.products.size
+  end
 end
